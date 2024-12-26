412a657f035959678bba5d71110e4996e87ce000
Kuldeep Mehar <kuldeepmehar34@gmail.com>
2024-12-26 13:20:21 +0530

    Tic-Tac-Toe game in React-js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+// import Header from "./component/Header";
+import Board from "./Component/Board";
+
+const App = () => {
+  return (
+    <div className="App">
+      < Board />
+    </div>
+  )
+};
+
+export default App;
